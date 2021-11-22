6b8162ea7600cd9a2020c82a6df3d177493be632
Farzin Jabbary <farzin.jabbary@gmail.com>
2021-11-21 23:23:53 -0500

    form validation and no result added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { FetchDocs } from './utils'
 import { useState } from 'react'
 import Loading from './components/Loading'
 import Header from './components/Header'
+import classnames from 'classnames'
 
 function App() {
   const [query, setQuery] = useState('')
+  const [hasResult, setHasResult] = useState('')
   const [queryResult, setQueryResult] = useState('')
   const [books, setBooks] = useState([])
   const [loading, setLoading] = useState(false)
   const [sortBy, setSortBy] = useState('title')
+  const [errors, setErrors] = useState({ queryError: '' })
 
+  const changeHandler = (e) => {
+    setQuery(e.target.value)
+    e.target.value.length > 0 ? setErrors(false) : setErrors(true)
+  }
 
   const sortByAttr = (attr) => {
     const sortedBooks = books.sort((a, b) => a[attr] > b[attr] ? 1 : -1)
@@ -19,11 +26,15 @@ function App() {
   }
 
   const searchBooks = async () => {
-    if (query === '' || query === null) return
+    if (query === '' || query === null) {
+      setErrors({ queryError: 'Please enter valid value' })
+      return
+    }
 
     setLoading(true)
 
     const docs = await FetchDocs(query)
+    docs.length !== 0 ? setHasResult('true') : setHasResult('false')
 
     setBooks(
       docs
@@ -39,6 +50,7 @@ function App() {
     setLoading(false)
     setQueryResult(query)
     setQuery("")
+    setErrors({})
   }
 
   if (loading) return <Loading />
@@ -53,9 +65,11 @@ function App() {
             <input
               aria-label="search-query"
               value={query}
-              onChange={e => setQuery(e.target.value)}
+              onChange={changeHandler}
               placeholder="Plase input the query"
+              className={classnames({ 'error': errors.queryError })}
             />
+            <p className="text-error"><small>{errors.queryError}</small></p>
             <button type="button" onClick={searchBooks}>Search</button>
           </div>
 
@@ -74,23 +88,27 @@ function App() {
         </div>
 
         <div className="book-container" data-testid='books'>
-          {books.length !== 0 ? <h1 className="results-text">Found ({books.length}) results for {queryResult}</h1> : ""}
-          <div className="book-list">
-            {books.map((book, index) => (
-              <div className="book d-flex" key={index}>
-                <div className="book-details">
-                  <p><strong>Title:</strong> {book.title}</p>
-                  <p><strong>Author:</strong> {book.title}</p>
-                  <p><strong>Published Date:</strong> {book.published_date}</p>
-                </div>
-                <div>
-                  <div className="book-cover">
-                    <img src={book.cover} width="100%" alt="Cover" />
+          {hasResult === 'true' && <div>
+            <h1 className="results-text">Found ({books.length}) results for "{queryResult}"</h1>
+            <div className="book-list">
+              {books.map((book, index) => (
+                <div className="book d-flex" key={index}>
+                  <div className="book-details">
+                    <p><strong>Title:</strong> {book.title}</p>
+                    <p><strong>Author:</strong> {book.title}</p>
+                    <p><strong>Published Date:</strong> {book.published_date}</p>
+                  </div>
+                  <div>
+                    <div className="book-cover">
+                      <img src={book.cover} width="100%" alt="Cover" />
+                    </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          </div>}
+
+          {hasResult === 'false' && <h1 className="results-text">No Result Found for "{queryResult}"</h1>}
         </div>
       </div>
     </div>
